293ebcd9b775269a2dd4d09e59272207f309a35a
Jordan Huynh <121647933+jrdnhynh@users.noreply.github.com>
2024-02-22 19:35:24 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,45 @@
+"use strict";
+
+$(document).ready(() => {
+    // fade in animation for the resume container 1.2 seconds
+    $('.resume-container').fadeIn(1200);
+
+    // mouse hover function
+    $('.resume-content p, .resume-content h1, .resume-content h2, .resume-content li, .label, .course, .technologies, .grade').hover(
+        function() {
+            $(this).css('background-color', 'rgba(0, 0, 149, 0.2)'); // change background color to purple
+        },
+        function() {
+            $(this).css('background-color', ''); // revert background color to original when mouse leaves
+        });
+
+    // click copy email button
+    $('#copy-email').click(() => {
+        // select the email text
+        const emailText = document.getElementById('email');
+        const range = document.createRange();
+        range.selectNode(emailText);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+
+        // copy the email text
+        document.execCommand('copy');
+        window.getSelection().removeAllRanges();
+
+        // display copied message briefly
+        emailText.innerText = 'Email copied!';
+        setTimeout(() => {
+            emailText.innerText = 'jrdnhynh@gmail.com';
+        }, 1500);
+    });
+
+    //opacity change on hover for the linkedin and github icon
+    $('.icons-container .linkedin-icon, .icons-container .github-icon, .copy-icon, .back-button').hover(
+        function() {
+            $(this).css('opacity', '0.5'); // change opacity on hover 50%
+        },
+        function() {
+            $(this).css('opacity', ''); // revert opacity to original when mouse leaves
+        }
+    );
+});
